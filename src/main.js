3daa8ca166590de60f5fab0a1bb5a15381c047fa
chenyu <chenyuhero@hotmail.com>
2017-11-04 14:48:17 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import './assets/reset.scss'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-Vue.use(ElementUI)
 
 
 Vue.config.productionTip = false
